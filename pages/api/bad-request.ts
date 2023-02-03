e878a41251800bd25329eda16a781bb271957c0e
Arnau-webDev <arnauma16@gmail.com>
2023-02-03 14:33:31 +0100

    Added simple middleware for general bad requests

--- pages/api/bad-request.ts
+++ pages/api/bad-request.ts
@@ -3,16 +3,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   ok: boolean,
-  message: string,
-  method: string
+  message: string | string[],
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-  res.status(200).json(
-    { 
-      ok: true,
-      message: 'Hey there!',
-      method: req.method || 'no method'
-    })
+  const { message = 'Bad Request' } = req.query;
+
+  res.status(400).json({ ok: false, message });
 };
